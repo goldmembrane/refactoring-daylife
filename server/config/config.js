fb032b05695b281f80edea6c9c0adbbbc2b2da1c
wonjun0120 <wonjundero@naver.com>
2020-07-01 16:42:17 +0900

    Add remark

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: 'root',
-    password: process.env.DATABASE_PASSWORD,
+    password: process.env.DATABASE_PASSWORD, //export DATABASE_PASSWORD=your_password_here
     database: 'daylife',
     host: 'localhost',
     dialect: 'mysql',
