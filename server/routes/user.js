52a9416b40d2a23e13be9ec527ce86d79f403acb
wonjun0120 <wonjundero@naver.com>
2020-07-03 11:52:12 +0900

    Fix worng words

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const { userController } = require('../controller');
 
-router.post('/signup', userController.siginup.post);
-router.post('/signin', userController.siginin.post);
-router.post('/signout', userController.siginout.post);
+router.post('/signup', userController.signup.post);
+router.post('/signin', userController.signin.post);
+router.post('/signout', userController.signout.post);
 router.get('/info',userController.info.get);
 
 module.exports = router;
