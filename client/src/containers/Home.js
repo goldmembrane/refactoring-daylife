e03f0eea992b7b8f99a47eeb16041227c7d8d429
goldmembrane <49430407+goldmembrane@users.noreply.github.com>
2020-07-07 16:20:13 +0900

    Delete Home.js
    delete localStorage

--- client/src/containers/Home.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import Home from '../pages/Home';
-
-const mapStateToProps = state => ({
-  date: state.setDateReducer.date
-});
-
-export default connect(mapStateToProps)(Home);
