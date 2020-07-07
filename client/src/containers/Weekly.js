8f263a556685372f235badbef5c35c269d6175ef
goldmembrane <49430407+goldmembrane@users.noreply.github.com>
2020-07-07 16:20:33 +0900

    Delete Weekly.js
    delete localStorage

--- client/src/containers/Weekly.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import Weekly from '../pages/Weekly';
-
-const mapStateToProps = state => ({
-  date: state.setDateReducer.date
-});
-
-export default connect(mapStateToProps)(Weekly);
