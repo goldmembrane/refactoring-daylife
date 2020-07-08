4429c06ae3164afcc20cb3f0977b40f89330c986
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-07-08 11:00:48 +0900

    base

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <BrowserRouter>
